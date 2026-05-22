f02d815a451d05404a4a3af42b7e5d87e5d4090d
Akshidhan Wijeratnam <wakshiwije@gmail.com>
2026-05-22 08:35:50 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+import './styles.scss';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const newsSwiper = new Swiper('.news-swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 24,
+  pagination: {
+    el: '.news-swiper .swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + ' swiper-pagination-bullet-custom"></span>';
+    }
+  },
+});
+
+const testimonialSwiper = new Swiper('.testimonial-swiper', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  navigation: {
+    nextEl: '.testimonial-swiper .swiper-button-next',
+    prevEl: '.testimonial-swiper .swiper-button-prev',
+  },
+});
